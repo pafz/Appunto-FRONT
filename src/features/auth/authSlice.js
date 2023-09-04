8235835b48869b942eb753a54fa65ad39bb69928
pafz <paferza@gmail.com>
2023-09-04 16:30:13 +0200

    1st part, like the notes

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+
+const initialState = {
+  user: null,
+  token: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+});
+
+export const register = createAsyncThunk('auth/register', async user => {
+  async user => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+});
+
+export default authSlice.reducer;
