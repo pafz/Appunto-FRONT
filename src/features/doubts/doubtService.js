da388eeb01ee32458edcdd0f7250ef2822f86793
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-05 19:51:53 +0200

    Added Doubts component

---
+++ src/features/doubts/doubtService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3001";
+
+const createDoubt = async (doubtData) => {
+    const res = await axios.post(`${API_URL}/doubts`, doubtData);
+    return res.data;
+};
+
+const doubtService = {
+    createDoubt,
+};
+
+export default doubtService;
