ff98ce16bcb96e00bb4654a1d35828c4e65dac5b
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-06 12:45:54 +0200

    Fixed errors in Doubts

--- src/components/Doubts/Doubts.jsx
+++ src/components/Doubts/Doubts.jsx
@@ -1,15 +1,55 @@
-import React, { useEffect } from "react";
-
-import { getAll } from "../../features/doubts/doubtsSlice";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import SingleDoubt from "../SingleDoubt/SingleDoubt";
+import { useToast } from "@chakra-ui/react";
+
+const Doubts = () => {
+    const [formData, setFormData] = useState({
+        topic: "",
+        question: "",
+    });
 
 const Doubts = () => {
     const dispatch = useDispatch();
+    const toast = useToast();
 
     useEffect(() => {
-        dispatch(getAll());
-    }, []);
+        if (isSuccess) {
+            toast({
+                title: "Éxito",
+                description: message,
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+        if (isError) {
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    }, [message, isSuccess, isError, toast]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(
+            createDoubt({
+                topic,
+                question,
+            })
+        );
+    };
 
     return (
         <div>
