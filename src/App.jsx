158cca25f86e1b6b276904980db364677813d883
pafz <paferza@gmail.com>
2023-09-12 12:20:46 +0200

    fixed conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Login from './components/Login/Login';
@@ -45,53 +44,5 @@ function App() {
       </BrowserRouter>
     </div>
   );
-=======
-import React from "react";
-import "./App.css";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
-import Doubts from "./components/Doubts/Doubts";
-import TeacherZone from "./guards/TeacherZone";
-import Teacher from "./components/Teacher/Teacher";
-import PrivateZone from "./guards/PrivateZone";
-import DoubtDetail from "./components/DoubtDetail/DoubtDetail";
-
-function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/profile"
-                        element={
-                            <PrivateZone>
-                                <Profile />
-                            </PrivateZone>
-                        }
-                    />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/doubts" element={<Doubts />} />
-                    <Route path="/doubts/:_id" element={<DoubtDetail />} />
-
-                    <Route
-                        path="/teacher"
-                        element={
-                            <TeacherZone>
-                                <Teacher />
-                            </TeacherZone>
-                        }
-                    />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
->>>>>>> 301d589da1806c2d00e0ae1c5480b4e38947e376
 }
 export default App;
--- src/features/doubts/doubtsSlice.js
+++ src/features/doubts/doubtsSlice.js
@@ -11,13 +11,8 @@ export const createDoubt = createAsyncThunk(
   'doubts/createDoubt',
   async doubtData => {
     try {
-<<<<<<< HEAD
       console.log(createDoubt);
       return await doubtService.createDoubt(doubtData);
-=======
-        console.log(createDoubt);
-        return await doubtService.createDoubt(doubtData);
->>>>>>> 301d589da1806c2d00e0ae1c5480b4e38947e376
     } catch (error) {
       console.error(error);
     }
@@ -31,21 +26,12 @@ export const getAll = createAsyncThunk('doubts/getAll', async () => {
     console.error(error);
   }
 });
-<<<<<<< HEAD
 export const getById = createAsyncThunk('doubts/getById', async _id => {
   try {
     return await doubtService.getById(_id);
   } catch (error) {
     console.error(error);
   }
-=======
-export const getById = createAsyncThunk("doubts/getById", async (_id) => {
-    try {
-        return await doubtService.getById(_id);
-    } catch (error) {
-        console.error(error);
-    }
->>>>>>> 301d589da1806c2d00e0ae1c5480b4e38947e376
 });
 
 export const getByTopic = createAsyncThunk('doubts/getByTopic', async topic => {
@@ -65,7 +51,6 @@ export const deleteDoubt = createAsyncThunk('doubts/deleteDoubt', async id => {
 });
 
 export const doubtsSlice = createSlice({
-<<<<<<< HEAD
   name: 'doubts',
   initialState,
   reducers: {},
@@ -97,39 +82,7 @@ export const doubtsSlice = createSlice({
         state.doubts = state.doubts.filter(doubt => doubt.id != action.payload);
       });
   },
-=======
-    name: "doubts",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-
-            .addCase(createDoubt.fulfilled, (state, action) => {
-                state.doubts = action.payload.doubts;
-                state.isLoading = false;
-            })
-            .addCase(getAll.fulfilled, (state, action) => {
-                state.doubts = action.payload.doubts;
-                state.isLoading = false;
-            })
-            .addCase(getAll.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(getById.fulfilled, (state, action) => {
-                state.doubt = action.payload.doubt;
-                state.isLoading = false;
-            })
-            .addCase(getById.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(getByTopic.fulfilled, (state, action) => {
-                state.doubts = action.payload;
-            })
-            .addCase(deleteDoubt.fulfilled, (state, action) => {
-                state.doubts = state.doubts.filter((doubt) => doubt.id != action.payload);
-            });
-    },
->>>>>>> 301d589da1806c2d00e0ae1c5480b4e38947e376
 });
 
 export default doubtsSlice.reducer;
+p;
