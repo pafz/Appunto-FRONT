60e3ec8cd83db582999a5bca2f5c934c4c49255c
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-09-13 20:47:02 +0200

    Some changes in several components

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,116 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { register, reset } from '../../features/auth/authSlice';
-import {
-  Button,
-  Card,
-  Input,
-  Link,
-  Text,
-  VStack,
-  useToast,
-} from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import { register, reset } from "../../features/auth/authSlice";
+import { Button, Card, Input, Text, VStack, useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
-  });
-  const { name, email, password, password2 } = formData;
-  const { message, isSuccess, isError } = useSelector(state => state.auth);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+    const { name, email, password, password2 } = formData;
+    const { message, isSuccess, isError } = useSelector((state) => state.auth);
 
-  const dispatch = useDispatch();
-  const toast = useToast();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const toast = useToast();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isSuccess) {
-      toast({
-        title: 'Éxito',
-        description: message,
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-    if (isError) {
-      toast({
-        title: 'Error',
-        description: message,
-        status: 'error',
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-    // dispatch(reset()); NOT NECESARY???
-  }, [message, isSuccess, isError, toast]);
+    useEffect(() => {
+        if (isSuccess) {
+            toast({
+                title: "Éxito",
+                description: message,
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+        if (isError) {
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+        // dispatch(reset()); NOT NECESARY???
+    }, [message, isSuccess, isError, toast]);
 
-  const onChange = e => {
-    setFormData(prevState => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-  const onSubmit = e => {
-    e.preventDefault();
-    // HERE toast()??? .error
-    if (password !== password2) {
-      return notification.error({
-        message: 'Error',
-        description: 'Passwords do not match',
-      });
-    } else {
-      dispatch(register(formData));
-      setTimeout(() => {
-        navigate('/register');
-      }, 3000);
-    }
-  };
-  return (
-    <Card w="30%" p="3%" mt="10%" ml="35%">
-      <form onSubmit={onSubmit}>
-        <VStack spacing="4" align="center">
-          <Text color="teal" as="b" fontSize="1.2em" mb="3%">
-            Bienvenid@
-          </Text>
-          <Input
-            type="text"
-            name="name"
-            placeholder="name"
-            value={name}
-            onChange={onChange}
-          />
-          <Input
-            type="email"
-            name="email"
-            placeholder="email"
-            value={email}
-            onChange={onChange}
-          />
-          <Input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={password}
-            onChange={onChange}
-          />
-          <Input
-            type="password"
-            name="password2"
-            placeholder="password"
-            value={password2}
-            onChange={onChange}
-          />
-          <Button mt="8%" colorScheme="teal" type="submit">
-            Register
-          </Button>
-        </VStack>
-      </form>
-    </Card>
-  );
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // HERE toast()??? .error
+        if (password !== password2) {
+            return notification.error({
+                message: "Error",
+                description: "Passwords do not match",
+            });
+        } else {
+            dispatch(register(formData));
+            setTimeout(() => {
+                navigate("/register");
+            }, 3000);
+        }
+    };
+    return (
+        <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+            <Card w="30%" p="3%" mt="10%" ml="35%" mb="8%" boxShadow="2xl">
+                <form onSubmit={onSubmit}>
+                    <VStack spacing="4" align="center">
+                        <Text color="teal" as="b" fontSize="1.2em" mb="3%">
+                            Bienvenid@
+                        </Text>
+                        <Input type="text" name="name" placeholder="name" value={name} onChange={onChange} />
+                        <Input type="email" name="email" placeholder="email" value={email} onChange={onChange} />
+                        <Input type="password" name="password" placeholder="password" value={password} onChange={onChange} />
+                        <Input type="password" name="password2" placeholder="password" value={password2} onChange={onChange} />
+                        <Button mt="8%" colorScheme="teal" type="submit">
+                            Regístrate
+                        </Button>
+                    </VStack>
+                </form>
+            </Card>
+        </motion.div>
+    );
 };
 export default Register;
--- src/components/SingleDoubt/SingleDoubt.jsx
+++ src/components/SingleDoubt/SingleDoubt.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import getImageURL from "../../app/utils";
+import { motion } from "framer-motion";
 import "./SingleDoubt.scss";
 
 const SingleDoubt = () => {
@@ -18,28 +19,30 @@ const SingleDoubt = () => {
         <Grid templateColumns="repeat(5, 1fr)" gap="4" pl="10%" pr="10%">
             {doubts.map((doubt) => (
                 <Link to={"/doubts/" + doubt._id} key={doubt._id}>
-                    <Card p="4" className="doubt-card">
-                        {user && (
-                            <>
-                                <Box pb="6%">
-                                    <Text as="b" fontSize="1.1em">
-                                        {user.name}
-                                    </Text>
-                                    <Text fontSize="0.8em" color="gray.500">
-                                        {user.role.toUpperCase()}
-                                    </Text>
-                                    {user.image && <Image src={getImageURL(user.image)} alt={user.name} h="50px" w="50px" objectFit="cover" borderRadius="full" />}
-                                </Box>
-                            </>
-                        )}
-                        <Box className="image-container">
-                            <Image src={getImageURL(doubt.imagePath)} h="200px" w="100%" objectFit="cover" borderRadius="8px" className="image-zoom" />
-                        </Box>
-                        <Text fontSize="xl" mt="2" pt="3%" pb="3%">
-                            <Tag colorScheme="teal">{doubt.topic ? doubt.topic.toUpperCase() : ""}</Tag>
-                        </Text>
-                        <Text>{doubt.question}</Text>
-                    </Card>
+                    <motion.div initial={{ scale: 1 }} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+                        <Card p="4" className="doubt-card">
+                            {user && (
+                                <>
+                                    <Box pb="6%">
+                                        <Text as="b" fontSize="1.1em">
+                                            {user.name}
+                                        </Text>
+                                        <Text fontSize="0.8em" color="gray.500">
+                                            {user.role.toUpperCase()}
+                                        </Text>
+                                        {user.image && <Image src={getImageURL(user.image)} alt={user.name} h="50px" w="50px" objectFit="cover" borderRadius="full" />}
+                                    </Box>
+                                </>
+                            )}
+                            <Box className="image-container">
+                                <Image src={getImageURL(doubt.imagePath)} h="200px" w="100%" objectFit="cover" borderRadius="8px" className="image-zoom" />
+                            </Box>
+                            <Text fontSize="xl" mt="2" pt="3%" pb="3%">
+                                <Tag colorScheme="teal">{doubt.topic ? doubt.topic.toUpperCase() : ""}</Tag>
+                            </Text>
+                            <Text>{doubt.question}</Text>
+                        </Card>
+                    </motion.div>
                 </Link>
             ))}
         </Grid>
